91e6526e4365b2b334ee46d9a28ff8ed9f991748
Udennaka Marktony <chukwudiudennaka@gmail.com>
2022-04-19 20:47:13 +0200

    added integration testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 // src/App.js
-
+import './nprogress.css';
 import React, { Component } from 'react';
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { getEvents, extractLocations } from './api';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: 32
+  }
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
   render() {
     return (
       <div className="App">
-         <EventList />
-         <CitySearch />
+         <EventList events={this.state.events} NumberOfEvents={this.state.NumberOfEvents}/>
+         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
          <NumberOfEvents />
       </div>
     );
