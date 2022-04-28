e3144f299314b958cc9d96b4be20e149e71e4f61
Udennaka Marktony <chukwudiudennaka@gmail.com>
2022-04-28 17:23:22 +0200

    added alert features

--- src/App.js
+++ src/App.js
@@ -6,25 +6,36 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
+import { WarningAlert } from './Alert';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    warningText: "",
   }
+
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        this.setState({ events, locations: extractLocations(events), warningText:"" });
+      } else {
+        getEvents().then((events) => {
+          if (this.mounted) {
+            this.setState({events, locations: extractLocations(events), warningText:"Oops You are currently offline. Events cannot be loaded"});
+          }
+        })
       }
     });
-  }
+  } 
+
 
   componentWillUnmount(){
     this.mounted = false;
   }
+  
   updateEvents = (location, eventCount) => {
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
@@ -38,6 +49,7 @@ class App extends Component {
   render() {
     return (
       <div className="App"> 
+         <WarningAlert id='warningAlert' text={this.state.warningText} />
          <EventList events={this.state.events} NumberOfEvents={this.state.numberOfEvents}/>
          <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/>
          <NumberOfEvents numberOfEvents={this.state.numberOfEvents} 
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,70 +1,45 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
-    state = {
-        numberOfEvents: 32,
+  state = {
+    numberOfEvents: 32,
+    errorText: ''
+  };
+
+  handleInputChanged = event => {
+    let value = event.target.value;
+    if (value === '') { value = undefined };
+    // If value is negative or greater 32, display an error, else update list of events
+    if (value < 0 || value > 32) {
+      this.setState({
+        errorText: 'Select a number from 1 to 32'
+      })
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        errorText: ''
+      });
+      this.props.updatenumberOfEvents(value);
     }
+  };
+  
+  render() {
+    return (
 
-    handleInputChanged = (event) => {
-        const value = event.target.value;
-        if (value < 1 || value > 32) {
-          this.setState({
-            message: "Enter number between 1 and 32",
-          });
-        } else {
-          this.setState({
-            numberOfEvents: value,
-            message: "",
-          });
-        }
-        // this.props.updateNumberOfEvents(event.target.value);
-      };
+      <div className='numberOfEvents'>
+        <label>Number of events</label>
+        <input
+          type='number'
+          className='number'
+          placeholder='Number'
+          onChange={this.handleInputChanged}
+        />
+        <ErrorAlert id='errorAlert' text={this.state.errorText} />
+      </div>
 
-    render () {
-
-        return (
-            <div className='numberOfEvents'>
-                <input 
-                type='number'
-                className='inputNumberOfEvents'
-                // onChange={this.handleInputOnChange}
-                onChange={this.handleInputChanged}
-                value={this.state.numberOfEvents}
-                />
-            </div>
-        )
-    }
+    )
+  }
 }
+
 export default NumberOfEvents;
-
-
-
-// import React, { Component } from 'react';
-
-// class NumberOfEvents extends Component {
-//     state = {
-//         numberOfEvents: 32,
-//         eventCount: 32
-//     }
-
-//     handleInputOnChange = (eventCount) => {
-//         this.props.updateEvents(eventCount)
-//     }
-
-//     render () {
-//         const { eventCount } = this.state;
-
-//         return (
-//             <div className='numberOfEvents'>
-//                 <input 
-//                 type='number'
-//                 className='inputNumberOfEvents'
-//                 onChange={this.handleInputOnChange}
-//                 value={eventCount}
-//                 />
-//             </div>
-//         )
-//     }
-// }
-
-// export default NumberOfEvents;
